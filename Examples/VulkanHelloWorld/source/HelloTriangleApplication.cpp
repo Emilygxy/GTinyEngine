9163a5f14ab75f2794bf6e0d927a5cce4c7970fb
unknown <emily370203@126.com>
2025-07-13 10:59:15 +0800

    update framwork for tinyRenderer

--- Examples/VulkanHelloWorld/source/HelloTriangleApplication.cpp
+++ Examples/VulkanHelloWorld/source/HelloTriangleApplication.cpp
@@ -2,7 +2,7 @@
 #include <set>
 #include <algorithm>
 #include <fstream>
-#include "Helper.h"
+#include "ultis.h"
 #include <filesystem>
 
 namespace vulkan
---
+++ include/ultis.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "root_dir.h"
+#include<string>
+// use CMake defined micro path
+const std::string g_vk_shader_resource_path = VK_SHADER_RESOURCE_PATH;
+const std::string g_resource_path = RESOURCE_PATH;
+
