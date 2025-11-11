7653db51cbdbd2b3c801bdcf46051ee27f63b76d
emily <emily@gridworld.com>
2025-11-11 14:55:06 +0800

    add vulkan create_instance

--- Examples/VulkanPractice2/source/HelloTriangleApplication.cpp
+++ Examples/VulkanPractice2/source/HelloTriangleApplication.cpp
@@ -40,6 +40,11 @@ void HelloTriangleApp::mainLoop()
 
 void HelloTriangleApp::cleanUp()
 {
+	if (m_instance)
+	{
+		vkDestroyInstance(m_instance, nullptr);
+	}
+
 	if (m_window)
 	{
 		glfwDestroyWindow(m_window);
@@ -48,10 +53,10 @@ void HelloTriangleApp::cleanUp()
 	glfwTerminate();
 }
 
-void HelloTriangleApp::createInstance()
+void HelloTriangleApp::createInstance() 
 {
 	// optional, but for optimizer
-	VkApplicationInfo appInfo = {};
+	VkApplicationInfo appInfo{};
 	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
 	appInfo.pApplicationName = "Hello Triangle";
 	appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
@@ -60,21 +65,21 @@ void HelloTriangleApp::createInstance()
 	appInfo.apiVersion = VK_API_VERSION_1_0;
 
 	//neccassary, 
-	VkInstanceCreateInfo createInfo = {};
+	VkInstanceCreateInfo createInfo{};
 	createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
 	createInfo.pApplicationInfo = &appInfo;
 
-	//
 	uint32_t glfwExtensionCount = 0;
 	const char** glfwExtensions;
 	glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
 	createInfo.enabledExtensionCount = glfwExtensionCount;
 	createInfo.ppEnabledExtensionNames = glfwExtensions;
+
 	createInfo.enabledLayerCount = 0;
 	VkResult result = vkCreateInstance(&createInfo, nullptr, &m_instance);
-
 	if (result != VK_SUCCESS) {
-		throw std::runtime_error("HelloTriangleApp::createInstance failed to create instance!");
+		throw std::runtime_error("failed to create instance!");
 	}
 }
 
