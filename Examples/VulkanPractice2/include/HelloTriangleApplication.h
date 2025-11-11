9f179637ae8ef243762b2547ca21e67a889a56d2
emily <emily@gridworld.com>
2025-11-11 14:43:08 +0800

    add window setting for vulkan scene

--- Examples/VulkanPractice2/include/HelloTriangleApplication.h
+++ Examples/VulkanPractice2/include/HelloTriangleApplication.h
@@ -1,5 +1,6 @@
 #pragma once
-#include <vulkan/vulkan.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>  // will include 'vulkan.h'
 
 #include <iostream>
 #include <stdexcept>
@@ -9,14 +10,21 @@
 class HelloTriangleApp
 {
 public:
-	HelloTriangleApp();
+	HelloTriangleApp(uint16_t width = 1280u, uint16_t height = 800u);
 	~HelloTriangleApp();
 
 	void run();
 
 private:
+	void initWindow();
 	void initVulkan();
 	void mainLoop();
 	void cleanUp();
+
+private:
+	GLFWwindow* m_window{nullptr};
+
+	uint16_t m_width;
+	uint16_t m_height;
 };
 
--- Examples/VulkanPractice2/source/HelloTriangleApplication.cpp
+++ Examples/VulkanPractice2/source/HelloTriangleApplication.cpp
@@ -1,12 +1,24 @@
 #include "HelloTriangleApplication.h"
 
 
-HelloTriangleApp::HelloTriangleApp()
+HelloTriangleApp::HelloTriangleApp(uint16_t width , uint16_t height)
+	:m_width(width)
+	,m_height(height)
 {
 }
 
 HelloTriangleApp::~HelloTriangleApp()
 {
+}
+
+void HelloTriangleApp::initWindow()
+{
+	glfwInit();
+
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); // not to create OpenGL context
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE); 
+
+	m_window = glfwCreateWindow(m_width, m_height, "Vulkan-HelloTriangle", nullptr, nullptr);
 }
 
 void HelloTriangleApp::initVulkan()
@@ -15,14 +27,29 @@ void HelloTriangleApp::initVulkan()
 
 void HelloTriangleApp::mainLoop()
 {
+	if (!m_window)
+	{
+		return;
+	}
+
+	while (!glfwWindowShouldClose(m_window)) {
+		glfwPollEvents();
+	}
 }
 
 void HelloTriangleApp::cleanUp()
 {
+	if (m_window)
+	{
+		glfwDestroyWindow(m_window);
+	}
+
+	glfwTerminate();
 }
 
 void HelloTriangleApp::run()
 {
+	initWindow();
 	initVulkan();
 	mainLoop();
 
