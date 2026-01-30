8859b9883954496babb987641deaa4b9c5f89099
emily <emily@gridworld.com>
2026-01-30 19:06:39 +0800

    vk_VertixTriangle: Ch7-2:初识索引缓冲区

--- Examples/VK_VertixTriangle/include/EasyVulkan.h
+++ Examples/VK_VertixTriangle/include/EasyVulkan.h
@@ -181,4 +181,16 @@ namespace easy_vk {
                     deviceLocalBuffer::Recreate(size, VK_BUFFER_USAGE_VERTEX_BUFFER_BIT | otherUsages);
                 }
             };
+        class indexBuffer :public deviceLocalBuffer {
+        public:
+            indexBuffer() = default;
+            indexBuffer(VkDeviceSize size, VkBufferUsageFlags otherUsages = 0) :deviceLocalBuffer(size, VK_BUFFER_USAGE_INDEX_BUFFER_BIT | otherUsages) {}
+            //Non-const Function
+            void Create(VkDeviceSize size, VkBufferUsageFlags otherUsages = 0) {
+                deviceLocalBuffer::Create(size, VK_BUFFER_USAGE_INDEX_BUFFER_BIT | otherUsages);
+            }
+            void Recreate(VkDeviceSize size, VkBufferUsageFlags otherUsages = 0) {
+                deviceLocalBuffer::Recreate(size, VK_BUFFER_USAGE_INDEX_BUFFER_BIT | otherUsages);
+            }
+        };
 }
